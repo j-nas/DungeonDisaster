0f1a67ed0e51c622fae48b91f58d41e88ea9a8d6
Jon Naske <jon@shorecel.com>
2024-09-02 16:29:19 -0700

    init

---
+++ DungeonDisasters/Program.cs
@@ -0,0 +1,27 @@
+﻿using OpenAI;
+using OpenAI.Managers;
+using OpenAI.ObjectModels;
+using OpenAI.ObjectModels.RequestModels;
+using Microsoft.Extensions.Configuration;
+
+var config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+
+var openAiService = new OpenAIService(
+    new OpenAiOptions() { ApiKey = config["OpenAIServiceOptions:ApiKey"] }
+);
+
+var completionResult = await openAiService.ChatCompletion.CreateCompletion(
+    new ChatCompletionCreateRequest
+    {
+        Messages = new List<ChatMessage>
+        {
+            ChatMessage.FromSystem("You are a dungeons and dragons dungeon master"),
+            ChatMessage.FromUser("Start a new campaign"),
+        },
+        Model = Models.Gpt_4,
+    }
+);
+if (completionResult.Successful)
+{
+    Console.WriteLine(completionResult.Choices.First().Message.Content);
+}
